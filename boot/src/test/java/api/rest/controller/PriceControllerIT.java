40a87e67a1e5f218bf46be87bbcdac1932c28425
abel navarro <abelnavarro@MacBook-Pro-de-abel.local>
2024-06-12 17:00:31 +0200

    findApplicablePrice: added IT test

--- boot/src/test/java/api/rest/controller/PriceControllerIT.java
+++ boot/src/test/java/api/rest/controller/PriceControllerIT.java
@@ -26,30 +26,148 @@ class PriceControllerIT {
     @Autowired 
     private TestRestTemplate restTemplate;
     
+    
+    // Test 1: petición a las 10:00 del día 14 del producto 9e059d8f-e5b9-4f69-9238-4688e1bed548   para la brand 272595b8-0a72-4782-83db-5d66bd293120 (ZARA)
     @Test
-    void findApplicablePriceWhenFindRecordsMustReturn200Ok() {
+    void findApplicablePriceWhenFindRecordsMustReturn200OkCase1() {
         // Arrange
         final var requestHeaders = this.getHttpHeaders();
         final var requestEntity = new HttpEntity<>(requestHeaders);
         final UUID brandId = UUID.fromString("272595b8-0a72-4782-83db-5d66bd293120");
         final UUID productId = UUID.fromString("9e059d8f-e5b9-4f69-9238-4688e1bed548");
-        final var applicationDate = "2020-06-14T00:00:00";
+        final var applicationDate = "2020-06-14T10:00:00";
 
         final var endpoint = UriComponentsBuilder
                 .fromPath("/brands/{brandId}/products/{productId}/applicable-price")
                 .queryParam("applicationDate", applicationDate)
                 .buildAndExpand(brandId, productId)
                 .toUriString();
-        
+
         // Act
         final var response = this.restTemplate
                 .exchange(BASE_PATH.concat(endpoint), HttpMethod.GET, requestEntity, PriceDetail.class);
-        
+
         // Assert
         assertEquals(HttpStatus.OK, response.getStatusCode());
         assertNotNull(response.getBody());
         assertEquals(35.5, response.getBody().getPrice());
         assertEquals(1, response.getBody().getPriceCode());
+        assertEquals(brandId, response.getBody().getBrandId());
+        assertEquals(productId, response.getBody().getProductId());
+    }
+    
+    //Test 2: petición a las 16:00 del día 14 del producto 35455   para la brand 1 (ZARA)
+    @Test
+    void findApplicablePriceWhenFindRecordsMustReturn200OkCase2() {
+        // Arrange
+        final var requestHeaders = this.getHttpHeaders();
+        final var requestEntity = new HttpEntity<>(requestHeaders);
+        final UUID brandId = UUID.fromString("272595b8-0a72-4782-83db-5d66bd293120");
+        final UUID productId = UUID.fromString("9e059d8f-e5b9-4f69-9238-4688e1bed548");
+        final var applicationDate = "2020-06-14T16:00:00";
+
+        final var endpoint = UriComponentsBuilder
+                .fromPath("/brands/{brandId}/products/{productId}/applicable-price")
+                .queryParam("applicationDate", applicationDate)
+                .buildAndExpand(brandId, productId)
+                .toUriString();
+
+        // Act
+        final var response = this.restTemplate
+                .exchange(BASE_PATH.concat(endpoint), HttpMethod.GET, requestEntity, PriceDetail.class);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(25.45, response.getBody().getPrice());
+        assertEquals(2, response.getBody().getPriceCode());
+        assertEquals(brandId, response.getBody().getBrandId());
+        assertEquals(productId, response.getBody().getProductId());
+    }
+    
+    //Test 3: petición a las 21:00 del día 14 del producto 35455   para la brand 1 (ZARA)
+    @Test
+    void findApplicablePriceWhenFindRecordsMustReturn200OkCase3() {
+        // Arrange
+        final var requestHeaders = this.getHttpHeaders();
+        final var requestEntity = new HttpEntity<>(requestHeaders);
+        final UUID brandId = UUID.fromString("272595b8-0a72-4782-83db-5d66bd293120");
+        final UUID productId = UUID.fromString("9e059d8f-e5b9-4f69-9238-4688e1bed548");
+        final var applicationDate = "2020-06-14T21:00:00";
+
+        final var endpoint = UriComponentsBuilder
+                .fromPath("/brands/{brandId}/products/{productId}/applicable-price")
+                .queryParam("applicationDate", applicationDate)
+                .buildAndExpand(brandId, productId)
+                .toUriString();
+
+        // Act
+        final var response = this.restTemplate
+                .exchange(BASE_PATH.concat(endpoint), HttpMethod.GET, requestEntity, PriceDetail.class);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(35.50, response.getBody().getPrice());
+        assertEquals(1, response.getBody().getPriceCode());
+        assertEquals(brandId, response.getBody().getBrandId());
+        assertEquals(productId, response.getBody().getProductId());
+    }    
+    
+    //Test 4: petición a las 10:00 del día 15 del producto 35455   para la brand 1 (ZARA)
+    @Test
+    void findApplicablePriceWhenFindRecordsMustReturn200OkCase4() {
+        // Arrange
+        final var requestHeaders = this.getHttpHeaders();
+        final var requestEntity = new HttpEntity<>(requestHeaders);
+        final UUID brandId = UUID.fromString("272595b8-0a72-4782-83db-5d66bd293120");
+        final UUID productId = UUID.fromString("9e059d8f-e5b9-4f69-9238-4688e1bed548");
+        final var applicationDate = "2020-06-15T10:00:00";
+
+        final var endpoint = UriComponentsBuilder
+                .fromPath("/brands/{brandId}/products/{productId}/applicable-price")
+                .queryParam("applicationDate", applicationDate)
+                .buildAndExpand(brandId, productId)
+                .toUriString();
+
+        // Act
+        final var response = this.restTemplate
+                .exchange(BASE_PATH.concat(endpoint), HttpMethod.GET, requestEntity, PriceDetail.class);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(30.50, response.getBody().getPrice());
+        assertEquals(3, response.getBody().getPriceCode());
+        assertEquals(brandId, response.getBody().getBrandId());
+        assertEquals(productId, response.getBody().getProductId());
+    }
+
+    //Test 5: petición a las 21:00 del día 16 del producto 35455   para la brand 1 (ZARA)
+    @Test
+    void findApplicablePriceWhenFindRecordsMustReturn200OkCase5() {
+        // Arrange
+        final var requestHeaders = this.getHttpHeaders();
+        final var requestEntity = new HttpEntity<>(requestHeaders);
+        final UUID brandId = UUID.fromString("272595b8-0a72-4782-83db-5d66bd293120");
+        final UUID productId = UUID.fromString("9e059d8f-e5b9-4f69-9238-4688e1bed548");
+        final var applicationDate = "2020-06-16T21:00:00";
+
+        final var endpoint = UriComponentsBuilder
+                .fromPath("/brands/{brandId}/products/{productId}/applicable-price")
+                .queryParam("applicationDate", applicationDate)
+                .buildAndExpand(brandId, productId)
+                .toUriString();
+
+        // Act
+        final var response = this.restTemplate
+                .exchange(BASE_PATH.concat(endpoint), HttpMethod.GET, requestEntity, PriceDetail.class);
+
+        // Assert
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertNotNull(response.getBody());
+        assertEquals(38.95, response.getBody().getPrice());
+        assertEquals(4, response.getBody().getPriceCode());
         assertEquals(brandId, response.getBody().getBrandId());
         assertEquals(productId, response.getBody().getProductId());
     }
